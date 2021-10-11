f69bb3a57283117c230192967dd4fee1efed6cfa
jkmanye <jkmanye@jkmanyeui-MacBookAir.local>
2021-10-11 11:04:06 +0900

    Updated Some Shits XD

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const _ = require('lodash');
 const PORT = process.env.PORT;
 
 var bodyParser = require('body-parser');
@@ -11,20 +10,31 @@ app.use(bodyParser.urlencoded({extended: true}));
 var accRawdata = fs.readFileSync('accounts.json');
 var accounts = JSON.parse(accRawdata);
 
+var checkinRawdata = fs.readFileSync('accounts.json');
+var checkinLists = JSON.parse(accRawdata);
+
 function reloadAccount() {
     accRawdata = fs.readFileSync('accounts.json');
     accounts = JSON.parse(accRawdata);
+}
+
+function reloadCheckinLists() {
+    checkinRawdata = fs.readFileSync('accounts.jsoncheckins.json');
+    checkinLists = JSON.parse(accRawdata);
 }
 
 function encryptHash(s, n) {
     const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const lower = 'abcdefghijklmnopqrstuvwxyz';
     let answer = '';
+    var isNumber = new Boolean(false);
 
     for (let i = 0; i < s.length; i++) {
         const str = s[i];
+        console.log(str);
 
-        if (isNaN(str)) {
+        if (!isNaN(str)) {
+            answer += String(Number(str) + 5);
             continue;
         } else if (str == ' ') {
             answer += ' ';
@@ -36,6 +46,7 @@ function encryptHash(s, n) {
         if (index >= upperOrLower.length) {
             index -= upperOrLower.length;
         }
+        console.log(upperOrLower[index]);
         answer += upperOrLower[index];
     }
 
@@ -46,7 +57,9 @@ app.get('/', (req, res) => {
     res.send("CovidCheckin Database Working!")
     console.log("A new client packet recieved.")
     reloadAccount()
+    reloadCheckinLists()
     console.log(accounts)
+    console.log
 });
 
 app.get('/status', (req, res) => {
@@ -82,6 +95,38 @@ app.post('/register/:email/:password/:name/:telnum', function(req, res) {
     console.log(telnum)
     const newUser = {
         "email": email,
+        "pw": encryptash(password, 5),
+        "name": name,
+        "telnum": telnum
+    }
+    console.log(JSON.stringify(newUser))
+    accounts.push(newUser)
+    fs.writeFile('./accounts.json', JSON.stringify(accounts), function (err) {
+        if (err) {
+            console.log('Error has occurred!')
+            console.dir(err)
+            return
+        }
+        console.log('File wrote.')
+        reloadAccount()
+       }
+    )
+    return res.status(201).json(accounts)
+});
+
+app.post('/loginCodeRequest/:id/:pw/', function (req, res) {
+    console.log("A new client packet recieved.")
+    console.log(req.body)
+    const email = req.params.email
+    console.log(email)
+    const password = req.params.password
+    console.log(password)
+    const name = req.params.name;
+    console.log(name)
+    const telnum = req.params.telnum
+    console.log(telnum)
+    const newUser = {
+        "email": email,
         "pw": encryptHash(password, 5),
         "name": name,
         "telnum": telnum
@@ -104,4 +149,5 @@ app.post('/register/:email/:password/:name/:telnum', function(req, res) {
 app.listen(PORT, () => {
     console.log("CovidCheckin DB is running in port 3000!")
     console.log(accounts)
+    console.log(checkinLists)
 });
